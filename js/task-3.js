9505d94d7831ba9b733aac3901050392e3cb4135
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2023-11-26 13:14:38 +0100

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,16 @@
 "use strict";
 
 function checkForSpam(message) {
-  message = message.toLowerCase();
-  if (message.includes("spam") || message.includes("sale")) {
-    return true;
-  } else {
-    return false;
-  }
+  return (
+    message.toLowerCase().includes("spam") ||
+    message.toLowerCase().includes("sale")
+  );
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
